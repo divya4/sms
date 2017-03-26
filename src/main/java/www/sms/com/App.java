12a998d60768b6b067b89e2b1791077062fc0426
divya4 <divyas3026@gmail.com>
2017-03-26 11:06:20 +0530

    ytdrdfgchvgikuyyufgdxfgc nvm

---
+++ src/main/java/www/sms/com/App.java
@@ -0,0 +1,13 @@
+package www.sms.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
